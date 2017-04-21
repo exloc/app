65eece16e58b00d24853bcdeb2b7e6c1ca43673e
Richard Davis <rdavis.bacs@gmail.com>
2017-04-21 09:49:41 -0400

    add code_example feature description

---
+++ spec/features/code_example_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "code example" do
+  xdescribe "create" do
+    # As a exloc user,
+    # I want to add a code example,
+    # So that others can view it.
+
+    # [ ] User adds a url to a git repository, and a title.
+    # [ ] The following files must be present in the repo:
+    #   - metadata.json
+    #   - index.md
+    # [ ] Upon creation, user is shown a successful message and brought to the
+    #   show page for the code example.
+    # [ ] Form is re-rendered with errors if creation is unsuccessful.
+  end
+
+  xdescribe "update" do
+    # As a exloc user,
+    # I want to edit a code example I have created,
+    # So that the most relevant and correct information is displayed.
+  end
+
+  xdescribe "delete" do
+    # As a exloc user,
+    # I want to remove a code example I have created,
+    # So that other exloc users aren't shown bad examples.
+  end
+end
